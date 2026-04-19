aa3093d19dec9799a96c6f43b00a8f8ea1f9b6aa
Giovanni Fioretto <giovanni.fioretto.23@hotmail.it>
2026-04-19 17:30:20 +0200

    Initial Commit

---
+++ KanaGuesser/KanaGuesserApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanaGuesserApp.swift
+//  KanaGuesser
+//
+//  Created by Giovanni Fioretto on 19/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct KanaGuesserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanaGuesserTests/KanaGuesserTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanaGuesserTests.swift
+//  KanaGuesserTests
+//
+//  Created by Giovanni Fioretto on 19/04/2026.
+//
+
+import Testing
+@testable import KanaGuesser
+
+struct KanaGuesserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
